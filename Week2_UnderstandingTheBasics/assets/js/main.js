9cdd367919620d0e7f2c664dd3cfc567d1a6b663
emilyleiyy <60151509+emilyleiyy@users.noreply.github.com>
2021-09-11 22:50:50 +0800

    week2

---
+++ Week2_UnderstandingTheBasics/assets/js/main.js
@@ -0,0 +1,73 @@
+// Create an array to store all questions.
+var allQuestions = [];
+// Store each question in an object.
+allQuestions[0] = {
+    question: "What color do you see",
+    choices: ["Black", "White","No color"],
+    correctAnswer: 2
+};
+allQuestions[1] = {
+    question: "Is this white in black? or black in white?",
+    choices: ["White in black", "Black in white","Blended"],
+    correctAnswer: 1
+};
+allQuestions[2] = {
+    question: "Do black and white exist independently of each other?",
+    choices: ["Yes", "No", "Not sure"],
+    correctAnswer: 0
+};
+
+// Display first question
+document.getElementById("question").textContent = allQuestions[0].question;
+
+document.getElementById("choice0").textContent = allQuestions[0].choices[0];
+
+document.getElementById("choice1").textContent = allQuestions[0].choices[1];
+
+document.getElementById("choice2").textContent = allQuestions[0].choices[2];
+
+// Create a variable to store the user's score
+var userScore = 0;
+
+// Create a variable to store the index of the current question
+var questionNum = 0;
+
+// When the NEXT button is clicked, the user's score is updated, the current question is hidden, and the next question is revealed.
+  $("#next").click(function() {
+   
+  // Check if User answered question.
+  // If so, update user's score. If not, do not continue quiz until answer is given.
+  if($("form input[name=answer]:checked").val() == allQuestions[questionNum].correctAnswer) {
+    userScore++;
+  }
+  
+  // If last question, show user's score rather than next question
+    if (questionNum == (allQuestions.length - 1)) {
+    document.getElementsByTagName("form")[0].style.display = "none";
+
+    if (userScore=1){
+        document.getElementById("question").textContent ="The color is amazing.";
+    }
+
+    if (userScore=2){
+        document.getElementById("question").textContent ="Color painted our world.";
+    }
+
+    if (userScore=3){
+        document.getElementById("question").textContent ="Let color to speak for us.";
+    }
+
+    }
+  
+  // Current question is not the last question, so increment the current question index
+    questionNum++;
+  
+  // Replace current question with next question
+    document.getElementById("question").textContent = allQuestions[questionNum].question;
+
+    document.getElementById("choice0").textContent = allQuestions[questionNum].choices[0];
+
+    document.getElementById("choice1").textContent = allQuestions[questionNum].choices[1];
+
+    document.getElementById("choice2").textContent = allQuestions[questionNum].choices[2];
+	});
